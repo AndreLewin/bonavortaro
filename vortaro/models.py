829f7193db232c81853a892ae8f532ac38af81a5
axolotl <andre.charles.lewin@gmail.com>
2017-01-20 21:36:41 +0100

    Mi kreis la du bazajn modelojn: radiko k propono

--- vortaro/models.py
+++ vortaro/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Radiko(models.Model):
+    radikaro = models.CharField(max_length=200) # TODO : Devas esti aro de radikoj
+    poroj = models.FloatField()
+    malporoj = models.FloatField()
+
+    oficialeco = models.CharField(max_length=2)
+    duolingeco = models.BooleanField(default=False)
+    rango = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.radikaro
+
+class Propono(models.Model):
+    por = models.ForeignKey('vortaro.Radiko', related_name='proponoj')
+    per = models.ForeignKey('vortaro.Radiko', related_name='proponojDe')
+
+    poroj = models.FloatField()
+    malporoj = models.FloatField()
