67c32c90e5f4abddf19e103bdbe9e5aea0d51a0c
axolotl <andre.charles.lewin@gmail.com>
2017-01-22 20:49:11 +0100

    aperigis la radikojn en la ĉefpaĝo

--- vortaro/models.py
+++ vortaro/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 
 class Radiko(models.Model):
-    radikaro = models.CharField(max_length=200) # TODO : Devas esti aro de radikoj
-    poroj = models.FloatField()
-    malporoj = models.FloatField()
+    eraro = models.CharField(max_length=200) # TODO : Nun teksto, eble poste aro de radikoj
+    poroj = models.FloatField(default=0)
+    malporoj = models.FloatField(default=0)
 
-    oficialeco = models.CharField(max_length=2)
+    oficialeco = models.CharField(max_length=2, default='X')
     duolingeco = models.BooleanField(default=False)
-    rango = models.IntegerField(default=0)
+    oftecrango = models.IntegerField(default=0)
 
     def __str__(self):
         return self.radikaro
--- vortaro/views.py
+++ vortaro/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Radiko
 
 # Create your views here.
 def ĉefpaĝo(request):
-    return render(request, 'vortaro/ĉefpaĝo.html', {})
+    radikoaro = Radiko.objects.order_by('malporoj') # Xoaro = array de Xo
+    return render(request, 'vortaro/ĉefpaĝo.html', {'radikoaro': radikoaro})
